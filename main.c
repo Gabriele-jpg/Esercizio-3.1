7cf5f195e5d90bf67c773e1ea68d8e1b009754aa
Gabriele <dioguardi.gabriele@istitutolevi.edu.it>
2024-09-25 14:27:47 +0200

    Esercizio 3.1

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int numero;
+    printf("Inserisci un numero intero: ");
+    scanf("%d", &numero);
+    if (numero % 2 == 0) {
+        printf("Il numero %d è pari.\n", numero);
+    } else {
+        printf("Il numero %d è dispari.\n", numero);
+    }
+    return 0;
+}
